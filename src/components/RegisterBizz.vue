133d46e8b7a07a69ea8d808c336f19a930148ab7
Alexis <ali.finney@gmail.com>
2018-01-22 10:18:46 -0700

    setting up registration page

---
+++ src/components/RegisterBizz.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <h1>Bizz Register</h1>
+
+      <input
+        type="email"
+        name="email"
+        v-model="email"
+        placeholder="email"/>
+        <br>
+      <input
+        type="email"
+        name="email"
+        v-model="email"
+        placeholder="email"/>
+        <br>
+        <button 
+        @click="register">
+        Register
+        </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationServices'
+export default {
+    data () {
+        return {
+            email = '',
+            password = ''
+        }
+    },
+    watch: {
+        email (value) {
+            console.log('email has changed', value)
+        }
+    },
+    methods: {
+        async register() {
+            const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            console.log('register btn was clicked')
+        }
+    },
+    mounted () {
+        console.log('Register Biz component mounted')
+    }
+}
+
+</script>
