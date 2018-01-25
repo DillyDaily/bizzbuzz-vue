5c72cc9315f272c471f397b2b5963621870094d0
Alexis <ali.finney@gmail.com>
2018-01-25 13:03:09 -0700

    setting up route for one message, deleted /register path

---
+++ src/components/OneMessage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>One Message</h1>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -10,28 +10,29 @@ import ProfileBizz from './components/ProfileBizz.vue';
 import ProfileBuzz from './components/ProfileBuzz.vue';
 import SendMessage from './components/SendMessage.vue';
 import Messages from './components/Messages.vue';
+import OneMessage from './components/OneMessage.vue';
 import EditBizz from './components/EditBizz.vue';
 import EditBuzz from './components/EditBuzz.vue';
 import Login from './components/Login.vue';
 
 export default [
-    { path: '/', component: SplashPage },                           //SplashPage
-    { path: '/register', component: RegisterBizz },                 //Register Bizz
-    { path: '/register/bizz', component: RegisterBizz },            //Register Bizz
-    { path: '/register/buzz', component: RegisterBuzz },            //Register Buzz
-    // { path: '/bizz/profile/new', component: CreateProfileBizz },    //Create Profile Bizz
-    // { path: '/buzz/profile/new', component: CreateProfileBuzz },    //Create Profile Buzz
-    { path: '/success', component: ProfileSuccess },                //Success Creating Profile
-    { path: '/businesses', component: BusinessList },               //Get All Bizz
-    { path: '/influencers', component: InfluencerList },            //Get All Buzz
-    { path: '/profile/bizz/:id', props: true, component: ProfileBizz },          //Get One Bizz
-    { path: '/profile/buzz/:id', component: ProfileBuzz },          //Get One Buzz
+    { path: '/', component: SplashPage },                                         //SplashPage
+    // { path: '/register', component: RegisterBizz },                               //Register Bizz
+    { path: '/register/bizz', component: RegisterBizz },                          //Register Bizz
+    { path: '/register/buzz', component: RegisterBuzz },                          //Register Buzz
+    // { path: '/bizz/profile/new', component: CreateProfileBizz },               //Create Profile Bizz
+    // { path: '/buzz/profile/new', component: CreateProfileBuzz },               //Create Profile Buzz
+    { path: '/success', component: ProfileSuccess },                              //Success Creating Profile
+    { path: '/businesses', component: BusinessList },                             //Get All Bizz
+    { path: '/influencers', component: InfluencerList },                          //Get All Buzz
+    { path: '/profile/bizz/:id', props: true, component: ProfileBizz },           //Get One Bizz
+    { path: '/profile/buzz/:id', props: true, component: ProfileBuzz },           //Get One Buzz
 
-    { path: '/contact/:id', component: SendMessage },               //Send a Message
-    { path: '/my/messages/:id', component: Messages },              //Get All Own Messages
-    { path: '/message/:id', component: Messages },                  //Get One Message
+    { path: '/contact/:id', props: true, component: SendMessage },                //Send a Message
+    { path: '/my/messages/:id', props: true, component: Messages },               //Get All Own Messages
+    { path: '/message/:id', props: true, component: OneMessage },                 //Get One Message
 
-    { path: '/profile/bizz/edit/:id', component: EditBizz },        //Edit Own Bizz Profile
-    { path: '/profile/buzz/edit/:id', component: EditBuzz },        //Edit Own Buzz Profile
-    { path: '/login', component: Login },                           //Login
+    { path: '/profile/bizz/edit/:id', props: true, component: EditBizz },         //Edit Own Bizz Profile
+    { path: '/profile/buzz/edit/:id', props: true, component: EditBuzz },         //Edit Own Buzz Profile
+    { path: '/login', component: Login },                                         //Login
 ]
