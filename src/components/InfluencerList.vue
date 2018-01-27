fe32da31ecf6568eb293d9c352c2b759a72a54ff
Alexis <ali.finney@gmail.com>
2018-01-27 11:44:57 -0700

    able to get own profile & own messages page

--- src/components/InfluencerList.vue
+++ src/components/InfluencerList.vue
@@ -1,58 +1,100 @@
 <template>
   <div id="influencer-list">
-    <simple-nav></simple-nav>
-    <h1>Get All influencers</h1>
-    
-            <v-layout row wrap>
-            <v-flex xs12>
-            <v-card color="yellow darken-2" class="white--text">
-                <v-card-title primary-title>
-                 <div class="welcome">Wow, man. Look at all that Buzz...</div>
-                </v-card-title>
-            </v-card>
-            </v-flex>
-            </v-layout>
-            <div>{{ allInfluencers }} </div>
-        <div v-for="influencer in allInfluencers">BUZZ: {{ influencer.personal_brand }} </div>
-        <div v-for="images in allInfluencers">IMAGE<img class="pics" :src="images.image"/></div>
-        <div v-for="descriptions in allInfluencers">Description: {{ descriptions.description }}</div>
-    
+    <nav-bar-logged-in></nav-bar-logged-in>
+    <v-container column>
+      <v-layout class="shadow" row wrap>
+        <v-flex xs12>
+          <v-card color="yellow darken-2" class="white--text">
+            <v-card-title primary-title>
+              <div class="welcome">Hot dog! Look at all that buzz!</div>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm10 md6 v-for="influencer in allInfluencers" :key="influencer.id">
+          <v-card>
+            <v-container fluid grid-list-md>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-card-media id="pointer" @click="clickMethod(influencer.id)" :src="influencer.image" height="250px" width="250px" />
+                </v-flex>
+                <v-flex>
+                  <v-card-title primary-title>
+                    <div >
+                      <h3 class="buzzcards">{{ influencer.personal_brand }} </h3>
+                      <div class="description">What we're about: {{ influencer.description }} </div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn class="view" flat color="green" @click="clickMethod(influencer.id)">
+                      <v-icon large left light>account_circle</v-icon>
+                      View Profile</v-btn>
+                  </v-card-actions>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
   </div>
 </template>
 
-<script>
-import axios from 'axios';
-import SimpleNav from './SimpleNav.vue';
-export default {
-    data () {
-        return {
-          influencers: []
-      }
-    },
+<script scoped>
+  import axios from 'axios';
+  import NavBarLoggedIn from './NavBarLoggedIn.vue';
+
+  export default {
+    data: () => ({
+
+    }),
     components: {
-      'simple-nav': SimpleNav
+      'nav-bar-logged-in': NavBarLoggedIn
     },
     methods: {
-      
+      clickMethod(id) {
+        this.$router.push('/profile/buzz/' + id)
+      }
     },
     computed: {
-        allInfluencers() {
-            return this.$store.state.influencers
-        }
+      allInfluencers() {
+        console.log("allInfluencers", this.$store.state.influencers)
+        return this.$store.state.influencers
+      }
     },
-    created () {
-        this.$store.dispatch('LOAD_INFLUENCERS_LIST')
+    created() {
+      this.$store.dispatch('LOAD_INFLUENCERS_LIST')
     },
-
-    mounted () {
-        // axios.get('http://localhost:8000/influencers')
-        // .then((response) => {
-        //     console.log('axios response: ', response.data)
-        //     this.Influencers = response.data;
-        // })
-        // console.log('BUZZList component mounted')
-    }
-}
+    mounted() {}
+  }
 
 </script>
 
+<style scoped>
+  .pics {
+    height: 250px;
+    width: 250px;
+  }
+
+  #pointer {
+    cursor: pointer;
+  }
+
+  .welcome {
+    font-family: 'Shadows Into Light Two', cursive;
+    font-size: 3em;
+  }
+
+  .buzzcards {
+    font-family: 'Cabin Sketch', cursive;
+  }
+  .description {
+    font-family: 'Montserrat', sans-serif;
+  }
+  /* .shadow{
+    padding: 20px;
+    -webkit-box-shadow: 3px 4px 19px -4px rgba(84,83,84,1);
+    -moz-box-shadow: 3px 4px 19px -4px rgba(84,83,84,1);
+    box-shadow: 3px 4px 19px -4px rgba(84,83,84,1);
+  } */
+</style>
