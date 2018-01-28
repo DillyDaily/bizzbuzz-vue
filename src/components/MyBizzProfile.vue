29f8fae627e9e3413f03585abe5d21d830d7c5e0
Alexis <ali.finney@gmail.com>
2018-01-28 10:34:38 -0700

    building MyProfile pages

--- src/components/MyBizzProfile.vue
+++ src/components/MyBizzProfile.vue
@@ -1,21 +1,81 @@
 <template>
-  <div>
-    <h1>This is my Profile</h1>
-
-    <v-dialog width="350px" persistent>
-      <v-btn fab accent slot="activator" >
-        <v-icon>edit</v-icon>
-      </v-btn>
-      <v-card>
-        <v-container>
+  <div id="influencer-list">
+    <nav-bar-logged-in></nav-bar-logged-in>
+    <div class="flowers">
+      <div id="grey">
+        <v-container grid-list-md text-xs-center>
           <v-layout row wrap>
             <v-flex xs12>
-              <v-card-title>Edit Profile</v-card-title>
+              <v-card color="yellow darken-2" class="white--text">
+                <v-card-title primary-title>
+                  <div class="welcome">This is my Profile</div>
+                  <v-spacer></v-spacer>
+                    <v-dialog width="350px" persistent>
+                          <v-btn class="grey" fab accent slot="activator" >
+                            <v-icon>edit</v-icon>
+                          </v-btn>
+                          <v-card>
+                            <v-container>
+                              <v-layout row wrap>
+                                <v-flex xs12>
+                                  <v-card-title>Edit Profile</v-card-title>
+                                </v-flex>
+                              </v-layout>
+                              <v-divider></v-divider>
+                            </v-container>
+                          </v-card>
+                        </v-dialog>
+
+                </v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex xs6>
+              <v-card dark color="secondary">
+                <v-card-text class="px-0">PICTURE</v-card-text>
+                <!-- <v-card-media id="pointer" @click="clickMethod(business.id)" :src="business.image" height="250px" width="250px" /> -->
+              </v-card>
+            </v-flex>
+            <v-flex xs6>
+              <v-card dark color="secondary">
+                <v-card-text class="px-0">TEXT</v-card-text>
+              </v-card>
             </v-flex>
           </v-layout>
-          <v-divider></v-divider>
         </v-container>
-      </v-card>
-    </v-dialog>
+      </div>
+    <foot></foot>
+  </div>
   </div>
 </template>
+
+<script>
+import NavBarLoggedIn from './NavBarLoggedIn.vue';
+import Foot from './Foot.vue';
+import axios from 'axios';
+
+export default {
+    data: () => ({
+
+    }),
+    components: {
+      'nav-bar-logged-in': NavBarLoggedIn,
+      'foot': Foot
+    },
+}
+
+</script>
+
+
+<style scoped>
+  #grey {
+    background-color: rgba(255, 255, 255, 0.637);
+    background-size: cover;
+    } 
+  .flowers {
+    background-image: url("../assets/beeplant.jpg");
+    background-size: cover;
+  } 
+  #pointer {
+    cursor: pointer;
+  }
+</style>
--- src/components/MyBuzzProfile.vue
+++ src/components/MyBuzzProfile.vue
@@ -2,7 +2,20 @@
   <div>
     <nav-bar-logged-in-buzz></nav-bar-logged-in-buzz>
     <h1>This is my Profile</h1>
-
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card dark color="primary">
+              <v-card-text class="px-0">12</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs6 v-for="i in 2" :key="i">
+            <v-card dark color="secondary">
+              <v-card-text class="px-0">6</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     <v-dialog width="350px" persistent>
       <v-btn fab accent slot="activator" >
         <v-icon>edit</v-icon>
